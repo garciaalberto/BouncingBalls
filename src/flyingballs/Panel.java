39d03d18a3fff976f1b2cefd800dd69c0018df97
garciaalberto <Bertooooo10@gmail.com>
2019-05-23 11:47:16 +0200

    Trying to respect the single responsibility principle

--- src/flyingballs/Panel.java
+++ src/flyingballs/Panel.java
@@ -65,8 +65,8 @@ public class Panel extends JPanel implements MouseMotionListener{
                 graph.fillOval((int)ball.getPosition().getX(), (int)ball.getPosition().getY(), ball.getDiameter(), ball.getDiameter());
             }
         }
-        catch(Exception e){
-            
+        catch(Exception exception){
+            System.err.println("Be careful playing with so many balls");
         }
         
     }
@@ -75,83 +75,99 @@ public class Panel extends JPanel implements MouseMotionListener{
         for (Ball ball : balls){
             
             if(walls && !followMouse){
-                if(ball.getPosition().getX() + ball.getAngle().getX() < 0){
-                    ball.getAngle().setX(1);
-                } else if (ball.getPosition().getX() + ball.getAngle().getX() > WIDTH - ball.getDiameter()){
-                    ball.getAngle().setX(-1);
-                } else if(ball.getPosition().getY() + ball.getAngle().getY() < 0){
-                    ball.getAngle().setY(1);
-                } else if (ball.getPosition().getY() + ball.getAngle().getY() > HEIGHT - ball.getDiameter()){
-                    ball.getAngle().setY(-1);
-                }
-                ball.getPosition().add(ball.getAngle().getX()*ball.getSpeed(), ball.getAngle().getY()*ball.getSpeed());
+                moveWallsNotFollow(ball);
             }
             if (!walls && !followMouse){
-                if(ball.getPosition().getX() + ball.getAngle().getX() < 0){
-                    ball.getPosition().setX(WIDTH-(int)ball.getPosition().getX());
-                } else if (ball.getPosition().getX() + ball.getAngle().getX() > WIDTH - ball.getDiameter()){
-                    ball.getPosition().setX(0);
-                } else if(ball.getPosition().getY() + ball.getAngle().getY() < 0){
-                    ball.getPosition().setY(HEIGHT-(int)ball.getPosition().getY());
-                } else if (ball.getPosition().getY() + ball.getAngle().getY() > HEIGHT - ball.getDiameter()){
-                    ball.getPosition().setY(0);
-                }
-                ball.getPosition().add(ball.getAngle().getX()*ball.getSpeed(), ball.getAngle().getY()*ball.getSpeed());
+                moveNotWallsNotFollow(ball);
             }
             if(!walls && followMouse){
-                if(ball.getPosition().getX() + ball.getAngle().getX() < 0){
-                    ball.getPosition().setX(WIDTH-(int)ball.getPosition().getX());
-                } else if (ball.getPosition().getX() + ball.getAngle().getX() > WIDTH - ball.getDiameter()){
-                    ball.getPosition().setX(0);
-                } else if(ball.getPosition().getY() + ball.getAngle().getY() < 0){
-                    ball.getPosition().setY(HEIGHT-(int)ball.getPosition().getY());
-                } else if (ball.getPosition().getY() + ball.getAngle().getY() > HEIGHT - ball.getDiameter()){
-                    ball.getPosition().setY(0);
-                }
-                if(ball.getPosition().getX() > mouseX && ball.getPosition().getY() > mouseY){
-                    ball.getPosition().substract(ball.getAngle().getX()*ball.getSpeed(), ball.getAngle().getY()*ball.getSpeed());
-                }
-                else if(ball.getPosition().getX() < mouseX && ball.getPosition().getY() < mouseY){
-                    ball.getPosition().add(ball.getAngle().getX()*ball.getSpeed(), ball.getAngle().getY()*ball.getSpeed());
-                }
-                else if(ball.getPosition().getX() < mouseX && ball.getPosition().getY() > mouseY){
-                    ball.getPosition().add(ball.getAngle().getX()*ball.getSpeed(), -ball.getAngle().getY()*ball.getSpeed());
-                }
-                else if(ball.getPosition().getX() > mouseX && ball.getPosition().getY() < mouseY){
-                    ball.getPosition().add(ball.getSpeed()*-ball.getAngle().getX(), ball.getAngle().getY()*ball.getSpeed());
-                }
+                moveNotWallsFollow(ball);
             }
             if(walls && followMouse){
-                if(ball.getPosition().getX() + ball.getAngle().getX() < 0){
-                    ball.getAngle().setX(1);
-                } else if (ball.getPosition().getX() + ball.getAngle().getX() > WIDTH - ball.getDiameter()){
-                    ball.getAngle().setX(-1);
-                } else if(ball.getPosition().getY() + ball.getAngle().getY() < 0){
-                    ball.getAngle().setY(1);
-                } else if (ball.getPosition().getY() + ball.getAngle().getY() > HEIGHT - ball.getDiameter()){
-                    ball.getAngle().setY(-1);
-                }
-                if(ball.getPosition().getX() > mouseX && ball.getPosition().getY() > mouseY){
-                    ball.getPosition().substract(ball.getAngle().getX()*ball.getSpeed(), ball.getAngle().getY()*ball.getSpeed());
-                    ball.getAngle().setX(1);
-                    ball.getAngle().setY(1);
-                }
-                else if(ball.getPosition().getX() < mouseX && ball.getPosition().getY() < mouseY){
-                    ball.getPosition().add(ball.getAngle().getX()*ball.getSpeed(), ball.getAngle().getY()*ball.getSpeed());
-                    ball.getAngle().setX(1);
-                    ball.getAngle().setY(1);
-                }
-                else if(ball.getPosition().getX() < mouseX && ball.getPosition().getY() > mouseY){
-                    ball.getPosition().add(ball.getAngle().getX()*ball.getSpeed(), -ball.getAngle().getY()*ball.getSpeed());
-                    ball.getAngle().setX(1);
-                    ball.getAngle().setY(1);
-                }
-                else if(ball.getPosition().getX() > mouseX && ball.getPosition().getY() < mouseY){
-                    ball.getPosition().add(ball.getSpeed()*-ball.getAngle().getX(), ball.getAngle().getY()*ball.getSpeed());
-                    ball.getAngle().setX(1);
-                    ball.getAngle().setY(1);
-                }
+                moveWallsFollow(ball);
             }
+        }
+    }
+    
+    private void moveWallsNotFollow(Ball ball){
+        if(ball.getPosition().getX() + ball.getAngle().getX() < 0){
+            ball.getAngle().setX(1);
+        } else if (ball.getPosition().getX() + ball.getAngle().getX() > WIDTH - ball.getDiameter()){
+            ball.getAngle().setX(-1);
+        } else if(ball.getPosition().getY() + ball.getAngle().getY() < 0){
+            ball.getAngle().setY(1);
+        } else if (ball.getPosition().getY() + ball.getAngle().getY() > HEIGHT - ball.getDiameter()){
+            ball.getAngle().setY(-1);
+        }
+        ball.getPosition().add(ball.getAngle().getX()*ball.getSpeed(), ball.getAngle().getY()*ball.getSpeed());
+    }
+    
+    private void moveNotWallsNotFollow(Ball ball){
+        if(ball.getPosition().getX() + ball.getAngle().getX() < 0){
+            ball.getPosition().setX(WIDTH-(int)ball.getPosition().getX());
+        } else if (ball.getPosition().getX() + ball.getAngle().getX() > WIDTH - ball.getDiameter()){
+            ball.getPosition().setX(0);
+        } else if(ball.getPosition().getY() + ball.getAngle().getY() < 0){
+            ball.getPosition().setY(HEIGHT-(int)ball.getPosition().getY());
+        } else if (ball.getPosition().getY() + ball.getAngle().getY() > HEIGHT - ball.getDiameter()){
+            ball.getPosition().setY(0);
+        }
+        ball.getPosition().add(ball.getAngle().getX()*ball.getSpeed(), ball.getAngle().getY()*ball.getSpeed());
+    }
+    
+    private void moveNotWallsFollow(Ball ball){
+        if(ball.getPosition().getX() + ball.getAngle().getX() < 0){
+            ball.getPosition().setX(WIDTH-(int)ball.getPosition().getX());
+        } else if (ball.getPosition().getX() + ball.getAngle().getX() > WIDTH - ball.getDiameter()){
+            ball.getPosition().setX(0);
+        } else if(ball.getPosition().getY() + ball.getAngle().getY() < 0){
+            ball.getPosition().setY(HEIGHT-(int)ball.getPosition().getY());
+        } else if (ball.getPosition().getY() + ball.getAngle().getY() > HEIGHT - ball.getDiameter()){
+            ball.getPosition().setY(0);
+        }
+        if(ball.getPosition().getX() > mouseX && ball.getPosition().getY() > mouseY){
+            ball.getPosition().substract(ball.getAngle().getX()*ball.getSpeed(), ball.getAngle().getY()*ball.getSpeed());
+        }
+        else if(ball.getPosition().getX() < mouseX && ball.getPosition().getY() < mouseY){
+            ball.getPosition().add(ball.getAngle().getX()*ball.getSpeed(), ball.getAngle().getY()*ball.getSpeed());
+        }
+        else if(ball.getPosition().getX() < mouseX && ball.getPosition().getY() > mouseY){
+            ball.getPosition().add(ball.getAngle().getX()*ball.getSpeed(), -ball.getAngle().getY()*ball.getSpeed());
+        }
+        else if(ball.getPosition().getX() > mouseX && ball.getPosition().getY() < mouseY){
+            ball.getPosition().add(ball.getSpeed()*-ball.getAngle().getX(), ball.getAngle().getY()*ball.getSpeed());
+        }
+    }
+    
+    private void moveWallsFollow(Ball ball){
+        if(ball.getPosition().getX() + ball.getAngle().getX() < 0){
+            ball.getAngle().setX(1);
+        } else if (ball.getPosition().getX() + ball.getAngle().getX() > WIDTH - ball.getDiameter()){
+            ball.getAngle().setX(-1);
+        } else if(ball.getPosition().getY() + ball.getAngle().getY() < 0){
+            ball.getAngle().setY(1);
+        } else if (ball.getPosition().getY() + ball.getAngle().getY() > HEIGHT - ball.getDiameter()){
+            ball.getAngle().setY(-1);
+        }
+        if(ball.getPosition().getX() > mouseX && ball.getPosition().getY() > mouseY){
+            ball.getPosition().substract(ball.getAngle().getX()*ball.getSpeed(), ball.getAngle().getY()*ball.getSpeed());
+            ball.getAngle().setX(1);
+            ball.getAngle().setY(1);
+        }
+        else if(ball.getPosition().getX() < mouseX && ball.getPosition().getY() < mouseY){
+            ball.getPosition().add(ball.getAngle().getX()*ball.getSpeed(), ball.getAngle().getY()*ball.getSpeed());
+            ball.getAngle().setX(1);
+            ball.getAngle().setY(1);
+        }
+        else if(ball.getPosition().getX() < mouseX && ball.getPosition().getY() > mouseY){
+            ball.getPosition().add(ball.getAngle().getX()*ball.getSpeed(), -ball.getAngle().getY()*ball.getSpeed());
+            ball.getAngle().setX(1);
+            ball.getAngle().setY(1);
+        }
+        else if(ball.getPosition().getX() > mouseX && ball.getPosition().getY() < mouseY){
+            ball.getPosition().add(ball.getSpeed()*-ball.getAngle().getX(), ball.getAngle().getY()*ball.getSpeed());
+            ball.getAngle().setX(1);
+            ball.getAngle().setY(1);
         }
     }
     
