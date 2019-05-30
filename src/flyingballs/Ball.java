4f2ef62a0fdf53d51325dd4f670460c1b3c48ad2
garciaalberto <Bertooooo10@gmail.com>
2019-05-30 19:34:53 +0200

    Code comented in spanish because reasons

--- src/flyingballs/Ball.java
+++ src/flyingballs/Ball.java
@@ -1,83 +1,82 @@
+/**
+ *
+ * @authors Korn, Andreas Manuel & García Socias, Alberto 
+ */
 package flyingballs;
 
 import java.awt.Color;
 import java.awt.Graphics;
 
-/**
- *
- * @authors Korn, Andreas Manuel & García Socias, Alberto 
- */
 public class Ball {
-    private final int DIAMETER = 35;
-    private final String SHAPE = "Circle";
-    private final int[] COLOR = new int[3];
-    private Vector speed;
-    private Vector position, acceleration;
+    private final int DIAMETER = 35; // Indita el diametro de la bola
+    private final String SHAPE = "Circle"; // String específicado por el enunciado, no es usado en ningún momento
+    private final int[] COLOR = new int[3]; // Indica el color de la bola con tres valores Integer (R, G y B)
+    private Vector speed, position, acceleration; // Indica los vectores de velocidad, posición y aceleración
 
-    public Vector getAcceleration() {
+    public Vector getAcceleration() { // Método que devuelve la aceleración
         return acceleration;
     }
     
-    public void setAcceleration(Vector acceleration){
+    public void setAcceleration(Vector acceleration){ // Método que establece la aceleración
         this.acceleration = acceleration;
     }
     
-    public void setAcceleration(double X, double Y){
+    public void setAcceleration(double X, double Y){ // Overload del método para establecer la aceleración que funciona con dos double
         this.acceleration.setX(X);
         this.acceleration.setY(Y);
     }
     
-    public int getDiameter() {
+    public int getDiameter() { // Método que devuelve el diametro de la bola
         return DIAMETER;
     }
 
-    public int[] getColor() {
+    public int[] getColor() { // Método que devuelve los valores RGB de la bola
         return COLOR;
     }
 
-    public Vector getSpeed() {
+    public Vector getSpeed() { // Método que devuelve la velocidad de la bola
         return speed;
     }
     
-    public void setSpeed(double X, double Y){
+    public void setSpeed(double X, double Y){ // Método para establecer la velocidad de la bola
         this.speed.add(X, Y);
     }
 
-    public Vector getPosition() {
+    public Vector getPosition() { // Método que devuelve la posición de la bola
         return position;
     }
     
-    public void setPosition(Vector position){
+    public void setPosition(Vector position){ // Método para establecer la posición de la bola
         this.position = position;
     }
     
-    public void setPosition(double X, double Y){
+    public void setPosition(double X, double Y){ // Overload del método para establecer la posición de la bola con dos double
         this.position.setX(X);
         this.position.setY(Y);
     }
     
-    public void accelerate(){
+    public void accelerate(){ // Método para acelerar la bola, añade a la velocidad la acceleración
         this.speed.add(acceleration);
     }
     
-    public void paintBall(Graphics graph){
-        graph.setColor(Color.BLACK);
-        graph.drawOval((int) this.getPosition().getX(), (int) this.getPosition().getY(), this.getDiameter(), this.getDiameter());
-        graph.setColor(new Color(this.getColor()[0], this.getColor()[1], this.getColor()[2]));
-        graph.fillOval((int) this.getPosition().getX(), (int) this.getPosition().getY(), this.getDiameter(), this.getDiameter());
+    public void paintBall(Graphics graph){ // Método que pinta la bola
+        graph.setColor(Color.BLACK); // Elige el color negro
+        graph.drawOval((int) this.getPosition().getX(), (int) this.getPosition().getY(), this.getDiameter(), this.getDiameter()); // Genera el círculo contenedor del color
+        graph.setColor(new Color(this.getColor()[0], this.getColor()[1], this.getColor()[2])); // Elige el color de la bola
+        graph.fillOval((int) this.getPosition().getX(), (int) this.getPosition().getY(), this.getDiameter(), this.getDiameter()); // Rellena el círculo con dicho color
     }
     
-    public Ball(){
-        this.position = new Vector((int)Math.floor(Math.random() * (1000-this.DIAMETER)), (int)Math.floor(Math.random() * (500-this.DIAMETER)));
+    public Ball(){ // Constructor para el objeto bola
+        this.position = new Vector((int)Math.floor(Math.random() * (1000-this.DIAMETER)), (int)Math.floor(Math.random() * (500-this.DIAMETER))); // Randomización de la posición
         double xSpd = Math.random()*2-1;
-        if(xSpd<0){
+        if(xSpd<0){ // Randomización de la dirección
             xSpd = -0.5;
         } else {
             xSpd = 0.5;
         }
-        this.speed = new Vector(xSpd, 0.5);
-        this.acceleration = new Vector(0, 1);
-        COLOR[0] = (int)Math.floor(Math.random() * 256);
+        this.speed = new Vector(xSpd, 0.5); // Ajuste de la velocidad de la bola
+        this.acceleration = new Vector(0, 1); // Ajuste de la acceleración de la bola
+        COLOR[0] = (int)Math.floor(Math.random() * 256); // Randomización de sus valores RGB
         COLOR[1] = (int)Math.floor(Math.random() * 256);
         COLOR[2] = (int)Math.floor(Math.random() * 256);
     }
--- src/flyingballs/FlyingBalls.java
+++ src/flyingballs/FlyingBalls.java
@@ -1,3 +1,7 @@
+/**
+ *
+ * @authors Korn, Andreas Manuel & García Socias, Alberto 
+ */
 package flyingballs;
 
 import java.awt.BorderLayout;
@@ -12,65 +16,60 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.text.BadLocationException;
 
-/**
- *
- * @author Korn, Andreas Manuel & García Socias, Alberto 
- */
-public class FlyingBalls extends JFrame {
+public class FlyingBalls extends JFrame { // FlyingBalls hereda de la clase JFrame
     
-    private static int balls = 10;
+    private static int balls = 2; // Inizializamos el programa con 2 bolas
     
-    public FlyingBalls(Panel panel, JPanel optionsMenu){
-        setPreferredSize(new Dimension(1300,540));
-        setLayout(new BorderLayout());
-        add(panel, BorderLayout.CENTER);
-        add(optionsMenu, BorderLayout.EAST);
-        pack();
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public FlyingBalls(Panel panel, JPanel optionsMenu){ // Creamos el JFrame con un constructor para la clase FlyingBalls
+        setPreferredSize(new Dimension(1300,540)); // Le damos unas dimensiones de 1300 de anchura (1000 para las bolas y 300 para las opciones) por 540 de altura)
+        setLayout(new BorderLayout()); // Crea un layout per alinear los dos JPanel
+        add(panel, BorderLayout.CENTER); // Añade el panel princnipal que contendrá las bolas en movimiento en el centro
+        add(optionsMenu, BorderLayout.EAST); // Añade un segundo panel que contendrá el menú de opciones en el este
+        pack(); // Empaqueta todo
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Cierra el programa cuando se cierra la ventana
     }
     
     public static void main(String[] args) throws InterruptedException, BadLocationException {
         
-        Panel panel = new Panel(10);
+        Panel panel = new Panel(2); // Crea un nuevo panel con las dos bolas
         
-        JPanel optionsMenu = new JPanel();
-        optionsMenu.setLayout(new BoxLayout(optionsMenu, 1));
+        JPanel optionsMenu = new JPanel(); // Creación de el panel de opciones
+        optionsMenu.setLayout(new BoxLayout(optionsMenu, 1)); // Creación de un BoxLayout para las opciones
         optionsMenu.setBackground(Color.LIGHT_GRAY);
-        JLabel label = new JLabel("<html><br><br><br><br><br># Balls</html>");
+        JLabel label = new JLabel("<html><br><br><br><br><br># Balls</html>"); // Aquí hemos usado HTML tags para centrar el menú verticalmente
         label.setFont(new Font("Arial", 0, 30));
         optionsMenu.add(label);
-        JTextField numberOfBalls = new JTextField("10", 17);
+        JTextField numberOfBalls = new JTextField("2", 17); // Field para el input de bolas
         numberOfBalls.setFont(new Font("Arial", 0, 20));
         numberOfBalls.setMaximumSize(new Dimension(550, 40));
         optionsMenu.add(numberOfBalls);
-        
-        JCheckBox walls = new JCheckBox("With walls");
+        JCheckBox walls = new JCheckBox("With walls"); // Checkbox para el input de pared
         walls.setFont(new Font("Arial", 0, 20));
         walls.setSelected(true);
         walls.setBackground(Color.LIGHT_GRAY);
         optionsMenu.add(walls);
-        JCheckBox followMouse = new JCheckBox("Follow mouse");
+        JCheckBox followMouse = new JCheckBox("Follow mouse"); // Checkbox para el input de seguir al ratón
         followMouse.setFont(new Font("Arial", 0, 20));
         followMouse.setBackground(Color.LIGHT_GRAY);
         optionsMenu.add(followMouse);
         
-        new FlyingBalls(panel, optionsMenu).setVisible(true);
+        new FlyingBalls(panel, optionsMenu).setVisible(true); // Creacion del JFrame con los dos paneles
         
         while(true){
-            panel.move();
-            panel.repaint();
-            Thread.sleep(10);
-            panel.setWalls(walls.isSelected());
-            panel.setFollowMouse(followMouse.isSelected());
+            panel.move(); // Llamamos al movimiento de las bolas
+            panel.repaint(); // Hacemos un repaint de las bolas en sus nuevas posiciones
+            Thread.sleep(10); // Dejamos reposar por 10 microsegundos
+            panel.setWalls(walls.isSelected()); // Damos a walls el valor que tenía anteriormente
+            panel.setFollowMouse(followMouse.isSelected()); // Damos a followMouse el valor que tenía anteriormente
             try{
-                if(Integer.parseInt(numberOfBalls.getText())!= balls){
-                    balls = Integer.parseInt(numberOfBalls.getText());
-                    panel.reset(balls);
+                if(Integer.parseInt(numberOfBalls.getText())!= balls){ // Si el número de bolas del panel de opciones no coinicde con las bolas que teníamos antes
+                    balls = Integer.parseInt(numberOfBalls.getText()); // Cambiamos el nuevo número de bolas
+                    panel.reset(balls); // Reseteamos el panel con el nuevo número de bolas
                 }
             }
-            catch(NumberFormatException exception){
-                balls = 0;
-                panel.reset(balls);
+            catch(NumberFormatException exception){ // Si el input en el menú no es un número Integer
+                balls = 0; // Pasa a tener 0 bolas
+                panel.reset(balls); // Se resetea el panel con 0 bolas
             }
         }
     }
